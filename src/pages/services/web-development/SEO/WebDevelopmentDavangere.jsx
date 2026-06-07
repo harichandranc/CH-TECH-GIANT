7f747655b1731d40510e8f1f6630a2d3ac66ea8d
Harichandran C <harichstech@gmail.com>
2026-06-07 16:32:42 +0530

    Added page

--- src/pages/services/web-development/SEO/WebDevelopmentDavangere.jsx
+++ src/pages/services/web-development/SEO/WebDevelopmentDavangere.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 import PageBanner from "../../../../components/PageBanner";
 import SectionWrapper from "../../../../components/SectionWrapper";
