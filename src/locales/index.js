0cf8911fd3386ef71c5da06983c1f9fe954beebd
Harichandran C <harichstech@gmail.com>
2026-08-04 19:46:13 +0530

    Update PRIVACY POLICY page

---
+++ src/locales/index.js
@@ -0,0 +1,31 @@
+import en from "./en";
+import zh from "./zh";
+
+export const translations = {
+  en,
+  zh,
+};
+
+export const categoryTranslations = {
+  en: {
+    All: "All",
+    Utilities: "Utilities",
+    Productivity: "Productivity",
+    Education: "Education",
+    Business: "Business",
+    Photography: "Photography",
+    Finance: "Finance",
+    PDF: "PDF",
+  },
+
+  zh: {
+    All: "全部",
+    Utilities: "实用工具",
+    Productivity: "效率工具",
+    Education: "教育",
+    Business: "商务",
+    Photography: "摄影",
+    Finance: "金融",
+    PDF: "PDF 工具",
+  },
+};
